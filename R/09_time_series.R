c0fe6866b1720d98903253555b8d4ca0080ecdeb
RuthCoronaMoreno <ruth.corona.m@gmail.com>
2022-07-21 10:57:52 -0500

    Dealing with time series, COVID-19 example

---
+++ R/09_time_series.R
@@ -0,0 +1,44 @@
+#Check https://scientific-computing.netlify.app/09_population_models/09_time_series.html
+
+
+library(dplyr)
+library(ggplot2)
+library(lubridate)
+library(zoo)
+
+
+covid <- read.csv("data/raw/covid19-dd7bc8e57412439098d9b25129ae6f35.csv")
+head(covid)
+
+# First checking the class
+class(covid$date)
+
+# Changing to date format
+covid$date <- as_date(covid$date) # require(lubridate)
+summary(covid$date)
+
+# Checking the class
+class(covid$date)
+
+# Now we can make numeric operations
+range(covid$date)
+
+ggplot(covid) +
+  geom_line(aes(x = date, y = new_confirmed)) +
+  theme_minimal()
+
+#Oops. We have negative cases and will substitute the negative values per zero.
+covid$new_confirmed[covid$new_confirmed < 0] <- 0
+
+#Rolling mean (Useful to show the tendency)
+covid$roll_mean <-zoo::rollmean(covid$new_confirmed,14, fill=NA)
+
+ggplot(covid) +
+  geom_line(aes(x = date, y = new_confirmed)) +
+  theme_minimal() +
+  scale_x_date(breaks="4 months", date_labels = "%Y-%m")+
+  labs(x = "Date", y = "New cases")+
+  geom_line(aes(x=date, y=roll_mean), color='red', size=1.2)
+
+
+
